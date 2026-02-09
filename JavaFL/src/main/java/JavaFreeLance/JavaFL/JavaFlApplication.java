79ffd8fd1914870a56619b5abe30252f2cf8840b
sudhirskp <gnitwallah@gmail.com>
2026-02-09 21:24:08 +0530

    Refactor Spring Boot application configuration by removing unnecessary exclusions

--- JavaFL/src/main/java/JavaFreeLance/JavaFL/JavaFlApplication.java
+++ JavaFL/src/main/java/JavaFreeLance/JavaFL/JavaFlApplication.java
@@ -2,10 +2,8 @@ package JavaFreeLance.JavaFL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class JavaFlApplication {
 
 	public static void main(String[] args) {
